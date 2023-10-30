a18beaa2093416e97dcf25b205566f382310702a
Наталія Малищенко <nataliia.malyshchenko@nure.ua>
2023-10-30 18:42:17 +0200

    minor fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,37 +18,10 @@ import { Subject, takeUntil } from 'rxjs';
 export class AppComponent implements OnDestroy {
   title = 'cat-gallery';
   unsubscribe: Subject<void> = new Subject<void>();
-  loading: boolean = true;
 
-  constructor(private router: Router, private route: ActivatedRoute) {
-    //  Subscribe to router events and handle them
-    this.router.events
-      .pipe(takeUntil(this.unsubscribe))
-      .subscribe((routerEvent) => {
-        this.checkRouterEvent(routerEvent as RouterEvent);
-      });
-  }
-
-  /** Handle router events and update the loading state accordingly. */
-  checkRouterEvent(routerEvent: RouterEvent): void {
-    // When a navigation starts, set the loading flag to true
-    if (routerEvent instanceof NavigationStart) {
-      this.loading = true;
-    }
-
-    // When a navigation ends, is canceled, or encounters an error, set the loading flag to false
-
-    if (
-      routerEvent instanceof NavigationEnd ||
-      routerEvent instanceof NavigationCancel ||
-      routerEvent instanceof NavigationError
-    ) {
-      this.loading = false;
-    }
-  }
+  constructor() {}
 
   ngOnDestroy(): void {
-    // Unsubscribe when the component is destroyed
     this.unsubscribe.next();
   }
 }
